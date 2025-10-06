01d1bae3bd4bc1cfbcef3b490852bc2e89f7858a
GiacomoDolci <giachimail@gmail.com>
2025-10-06 22:50:09 +0200

    feat: aggiunto componente attrazione

---
+++ frontend/src/app/components/attrazione/attrazione.ts
@@ -0,0 +1,29 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Api } from '../../services/api';
+
+@Component({
+  selector: 'app-attrazione',
+  standalone: false,
+  templateUrl: './attrazione.html',
+  styleUrls: ['./attrazione.css']
+})
+export class Attrazione implements OnInit {
+  attrazione: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private api: Api,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.api.getAttrazione(+id).subscribe(data => {
+        this.attrazione = data;
+        this.cdr.detectChanges();
+      });
+    }
+  }
+}
