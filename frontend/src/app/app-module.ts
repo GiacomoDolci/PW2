ffb1ae8cc04db92aa4023a967d7ed65739d690a5
GiacomoDolci <giachimail@gmail.com>
2025-10-06 22:53:56 +0200

    feat: aggiunti i componenti

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,22 +1,28 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Destinazioni } from './components/destinazioni/destinazioni';
 import { Attrazioni } from './components/attrazioni/attrazioni';
+import { Home } from './components/home/home';
+import { Attrazione } from './components/attrazione/attrazione';
 
 @NgModule({
   declarations: [
     App,
     Destinazioni,
-    Attrazioni
+    Attrazioni,
+    Home,
+    Attrazione
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
