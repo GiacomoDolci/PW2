54ddecbdbbeed3e0359639be27e3bb2ecaa4fea4
GiacomoDolci <giachimail@gmail.com>
2025-10-06 17:38:51 +0200

    feat: rotte api aggiunte

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DestinazioneController;
+use App\Http\Controllers\AttrazioneController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Rotte per le destinazioni
+Route::get('/destinazioni', [DestinazioneController::class, 'index']);
+Route::get('/destinazioni/{id}', [DestinazioneController::class, 'show']);
+
+// Rotte per le attrazioni
+Route::get('/attrazioni', [AttrazioneController::class, 'index']);
+Route::get('/attrazioni/{id}', [AttrazioneController::class, 'show']);
