c2f509e711e25a25347fb0ac7c1c72120f7d4fba
GiacomoDolci <giachimail@gmail.com>
2025-10-06 19:26:06 +0200

    feat: rotte

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Destinazioni } from './components/destinazioni/destinazioni';
+import { Attrazioni } from './components/attrazioni/attrazioni';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/destinazioni', pathMatch: 'full' },
+  { path: 'destinazioni', component: Destinazioni },
+  { path: 'destinazioni/:id', component: Attrazioni },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
