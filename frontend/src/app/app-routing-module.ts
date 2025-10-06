61bb551caa998c455152b25566b9b9559b88a007
GiacomoDolci <giachimail@gmail.com>
2025-10-06 22:54:09 +0200

    fix: sistemate le rotte

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './components/home/home';
 import { Destinazioni } from './components/destinazioni/destinazioni';
 import { Attrazioni } from './components/attrazioni/attrazioni';
+import { Attrazione } from './components/attrazione/attrazione';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/destinazioni', pathMatch: 'full' },
-  { path: 'destinazioni', component: Destinazioni },
+  { path: '', component: Home },
   { path: 'destinazioni/:id', component: Attrazioni },
+  { path: 'attrazioni/:id', component: Attrazione },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
